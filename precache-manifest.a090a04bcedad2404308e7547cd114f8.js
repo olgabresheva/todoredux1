d464c85e6f6a36e557daff9b7ef2f54ae7ec4191
olgab <olgabresheva7@gmail.com>
2020-08-19 00:57:55 -0700

    Updates

---
+++ precache-manifest.a090a04bcedad2404308e7547cd114f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90b04e71fedd684303d949e23a21b57f",
+    "url": "/todoredux1/index.html"
+  },
+  {
+    "revision": "80de831eaa2d490cf1dd",
+    "url": "/todoredux1/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "215922811f35618200c7",
+    "url": "/todoredux1/static/css/main.a3ae137b.chunk.css"
+  },
+  {
+    "revision": "80de831eaa2d490cf1dd",
+    "url": "/todoredux1/static/js/2.0911eb8c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todoredux1/static/js/2.0911eb8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "215922811f35618200c7",
+    "url": "/todoredux1/static/js/main.4fab34f2.chunk.js"
+  },
+  {
+    "revision": "2a911a3dcaf078edcc6d",
+    "url": "/todoredux1/static/js/runtime-main.e2282f7c.js"
+  }
+]);
